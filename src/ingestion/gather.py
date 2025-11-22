24c70053e47ce0fe6524f098f77f508ad1aba071
bglid <benjamin.h.glidden@gmail.com>
2025-11-22 14:25:52 -0500

    Adding SQL db gathering to gather.py

--- src/ingestion/gather.py
+++ src/ingestion/gather.py
@@ -47,8 +47,52 @@ def save_to_json(d):
 
 
 # upserting the Cluster and Opinion data to SQLdb
-def sql_db_upsert(cluster_json, opinion_json):
-    pass
+def sql_db_upsert(c_jn, o_jn):
+    """
+    Starting up DB session and then grabbing relevant metadata per record
+
+    Args:
+        c_jn: cluster json
+        o_jn: opinion json
+    """
+    db = SessionLocal()
+
+    # try getting an existing record, else create a new one
+    try:
+        cluster = db.get(Cluster, c_jn["id"])
+
+        # no cluster = new cluster
+        if not cluster:
+            cluster = Cluster(c_jn["id"])
+
+        # add cluster record data
+        cluster.case_name = c_jn.get("case_name")
+        cluster.docket_number = c_jn.get("docket_number")
+        cluster.date_filed = c_jn.get("date_filed")
+        cluster.citation = c_jn.get("citation")
+        cluster.absolute_url = c_jn.get("absolute_url")
+        cluster.precedential_status = c_jn.get("precedential_status")
+        cluster.raw_source = c_jn  # raw json
+
+        db.add(cluster)
+
+        # getting the opinion data
+        opinions = db.get(Opinions, o_jn["id"])
+        if not opinions:
+            opinions = Opinions(o_jn["id"])
+
+        # connecting to cluster
+        opinions.cluster_id = c_jn["id"]
+        opinions.author = o_jn.get("author")
+        opinions.raw_source = o_jn
+
+        db.add(opinions)
+
+        # saving changes
+        db.commit()
+    # close even if fail
+    finally:
+        db.close()
 
 
 def main():
@@ -59,7 +103,8 @@ def main():
 
     api = CourtListenerAPI()
 
-    query_r = api.call("search", params={"q": "copyright"})
+    # query_r = api.call("search", params={"q": "copyright"})
+    query_r = api.call("search", params={"q": "criminal"})
 
     if query_r.status_code == 200:
         q_data = query_r.json()
@@ -79,6 +124,10 @@ def main():
                 opinion_r = api.call(opinion)
 
                 save_to_json(opinion_r.json())
+
+                # also pushing to db
+                sql_db_upsert(cluster_r, opinion_r)
+
     else:
         print("Error:", query_r.status_code, query_r.text)
 

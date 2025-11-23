8f763f6ce3187ebc6eaff9e4dba4982eaef30d44
elnaske <naske.elias@outlook.com>
2025-11-23 10:27:53 -0500

    Update prompts.py
    - Prompts now loaded in from prompts.yaml (tested, works correctly)
    - Moved AGENT_ROLES and AVAILABLE_PROMPTS to __init__.py
    - Old functions commented out (to be deleted)
    - yaml_conversion.py commented out (to be deleted)

--- src/prompts/yaml_conversion.py
+++ src/prompts/yaml_conversion.py
@@ -1,39 +1,39 @@
-import yaml
-from prompts import get_all_prompts
+# import yaml
+# from prompts import get_all_prompts
 
-PATH = "./prompts/prompts.yaml" 
+# PATH = "./prompts/prompts.yaml" 
 
-def dump_to_yaml(prompts_dict):
-    with open(PATH, "w") as f:
-        yaml.dump(prompts_dict, f)
+# def dump_to_yaml(prompts_dict):
+#     with open(PATH, "w") as f:
+#         yaml.dump(prompts_dict, f)
 
-def test_yaml(prompts_dict):
-    with open(PATH, "r") as f:
-        yaml_loaded = yaml
+# def test_yaml(prompts_dict):
+#     with open(PATH, "r") as f:
+#         yaml_loaded = yaml
 
-    with open(PATH, "r") as f:
-        yaml_loaded = yaml.safe_load(f)
+#     with open(PATH, "r") as f:
+#         yaml_loaded = yaml.safe_load(f)
     
-    for key in prompts_dict.keys():
-        assert prompts_dict[key] == yaml_loaded[key]
-        print(key, "success")
+#     for key in prompts_dict.keys():
+#         assert prompts_dict[key] == yaml_loaded[key]
+#         print(key, "success")
 
-def str_presenter(dumper, data):
-    if "\n" in data:
-        return dumper.represent_scalar("tag:yaml.org,2002:str", data, style="|")
-    return dumper.represent_scalar("tag:yaml.org,2002:str", data)
-
-
-def main():
-    yaml.add_representer(str, str_presenter)
-
-    prompts_dict = get_all_prompts()
-
-    dump_to_yaml(prompts_dict)
-    test_yaml(prompts_dict)
-
-    print("success")
+# def str_presenter(dumper, data):
+#     if "\n" in data:
+#         return dumper.represent_scalar("tag:yaml.org,2002:str", data, style="|")
+#     return dumper.represent_scalar("tag:yaml.org,2002:str", data)
 
 
-if __name__ == "__main__":
-    main()
+# def main():
+#     yaml.add_representer(str, str_presenter)
+
+#     prompts_dict = get_all_prompts()
+
+#     dump_to_yaml(prompts_dict)
+#     test_yaml(prompts_dict)
+
+#     print("success")
+
+
+# if __name__ == "__main__":
+#     main()

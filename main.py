19673330c17acb397aaad7918a3264e17a24b765
elnaske <naske.elias@outlook.com>
2025-11-24 11:42:23 -0500

    Reformated agent code, moved main.py to root
    - Moved agent scripts into `agents.py`
    - Created `LegalAgent` superclass for agents to avoid repeated code
    - Added `config` arg to `run_agent_pipeline()`
    - Replaced `ChatOpenAI()` args with values from config
    - Added fields for `ChatOpenAI()` args to `config.yaml`
    - Added import statements to `agents/__init__.py`
    - Updated import statements from agents and prompts modules

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.utils import load_config
-from src.agents.agent_controller import run_agent_pipeline
+# from src.agents.agent_controller import run_agent_pipeline
+from src.agents import run_agent_pipeline
 
 def main():
     config = load_config()
@@ -7,7 +8,7 @@ def main():
     print("=== Legal-RAG Agent System ===")
     user_query = input("Ask a legal question:\n> ")
 
-    answer = run_agent_pipeline(user_query)
+    answer = run_agent_pipeline(user_query, config)
 
     print("\n=== Final Answer ===\n")
     print(answer)
--- src/agents/agent_controller.py
+++ src/agents/agent_controller.py
@@ -2,22 +2,30 @@ import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 
-from src.agents.prosecutor import ProsecutorAgent
-from src.agents.defendant import DefendantAgent
-from src.agents.judge import JudgeAgent
+# from src.agents.prosecutor import ProsecutorAgent
+# from src.agents.defendant import DefendantAgent
+# from src.agents.judge import JudgeAgent
 
-load_dotenv()
+from src.agents import ProsecutorAgent, DefendantAgent, JudgeAgent
 
-
-def run_agent_pipeline(user_question: str):
+def run_agent_pipeline(user_question: str, config: dict):
     print("\n=== Legal-RAG Agent System ===")
 
+    load_dotenv()
+
     llm = ChatOpenAI(
-        model="llama-3.1-8b-instant",
-        api_key=os.getenv("GROQ_API_KEY"),
-        base_url="https://api.groq.com/openai/v1",
-        temperature=0.3,
+        model=config["agents"]["model_name"],
+        api_key=os.getenv(config["agents"]["API_key_name"]),
+        base_url=config["agents"]["API_base_url"],
+        temperature=config["agents"]["temperature"],
     )
+
+    # llm = ChatOpenAI(
+    #     model="llama-3.1-8b-instant",
+    #     api_key=os.getenv("GROQ_API_KEY"),
+    #     base_url="https://api.groq.com/openai/v1",
+    #     temperature=0.3,
+    # )
 
     prosecutor = ProsecutorAgent(llm)
     defense = DefendantAgent(llm)
